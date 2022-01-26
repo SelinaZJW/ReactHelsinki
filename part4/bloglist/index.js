528673f8713a4a6c97973cc29f1b1431ee862902
郑佳雯 <zhengjiawen@192.168.1.119>
2022-01-26 19:53:05 +0000

    part4_bloglist_structured_backend

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,50 +1,62 @@
+const app = require('./app') // the actual Express application
 const http = require('http')
-// const config = require('./utils/config')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-// const logger = require('./utils/logger')
-// const middleware = require('./utils/middleware')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
+const server = http.createServer(app)
+
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
 
-const Blog = mongoose.model('Blog', blogSchema)
 
-//const config = require('./utils/config')
+// const http = require('http')
+// // const config = require('./utils/config')
+// const express = require('express')
+// const app = express()
+// const cors = require('cors')
+// const mongoose = require('mongoose')
+// // const logger = require('./utils/logger')
+// // const middleware = require('./utils/middleware')
 
-// logger.info(`Server running on port ${config.PORT}`)
+// const blogSchema = new mongoose.Schema({
+//   title: String,
+//   author: String,
+//   url: String,
+//   likes: Number
+// })
 
-const mongoUrl = 'mongodb+srv://part4:Zjwhy333333@cluster0.m4xp4.mongodb.net/bloglist?retryWrites=true&w=majority'
-mongoose.connect(mongoUrl)
+// const Blog = mongoose.model('Blog', blogSchema)
 
-app.use(cors())
-app.use(express.json())
+// //const config = require('./utils/config')
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
+// // logger.info(`Server running on port ${config.PORT}`)
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
+// const mongoUrl = 'mongodb+srv://part4:Zjwhy333333@cluster0.m4xp4.mongodb.net/bloglist?retryWrites=true&w=majority'
+// mongoose.connect(mongoUrl)
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+// app.use(cors())
+// app.use(express.json())
 
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+// app.get('/api/blogs', (request, response) => {
+//   Blog
+//     .find({})
+//     .then(blogs => {
+//       response.json(blogs)
+//     })
+// })
+
+// app.post('/api/blogs', (request, response) => {
+//   const blog = new Blog(request.body)
+
+//   blog
+//     .save()
+//     .then(result => {
+//       response.status(201).json(result)
+//     })
+// })
+
+// const PORT = 3003
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`)
+// })
